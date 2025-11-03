90e03a61dced6b3c7b3e9a74fa655ecc410456d8
andrewcunningham <andrew.cunningham@popsa.com>
2025-11-03 17:35:17 +0000

    Updates strings and typography

--- app/src/main/java/com/popsa/colorextraction/MainActivity.kt
+++ app/src/main/java/com/popsa/colorextraction/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.popsa.colorextraction.ui.theme.ColorExtractionTheme
 
 class MainActivity : ComponentActivity() {
 
